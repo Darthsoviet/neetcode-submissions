41088cf6dea00958dd9cbf2a525cb0c054691fab
Ivan Andres Alexis Del Villar Lujan <31465914+Darthsoviet@users.noreply.github.com>
2026-04-28 22:26:43 -0600

    Add: partition-equal-subset-sum - submission-6

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-6.py
@@ -0,0 +1,55 @@
+class Solution:
+
+
+
+    def canPartition(self, nums: List[int]) -> bool:
+        m ={}
+
+        def knap(i,c):
+            if m.get((i,c,)) != None:
+                return m.get((i,c,))
+            
+            if i >=len(nums):
+                return 0
+            
+
+            # (1, 3-5 = -2)
+            n_cap = c - nums[i]
+            zero = knap(i+1, c)
+
+            if nums[i] > c:
+                one = knap(i+1, c)
+
+            else:
+                one = knap(i+1, n_cap) + nums[i]
+            m[(i,c,)] = max(one,zero)
+            return m.get((i,c,))
+        
+        target = 0
+        for i in nums:
+            target += i
+        
+        if target % 2 ==1:
+            return False
+        else:
+            target = target // 2
+            x = knap(0,target)
+            return x == target
+        
+        
+
+
+
+
+
+
+            
+
+            
+
+            
+            
+
+            
+
+        
